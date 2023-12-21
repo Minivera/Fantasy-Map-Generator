493ad18626175228811c474bb9f83ad9ae17a534
Guillaume <guillaume.stpierre@docker.com>
2023-12-21 10:27:46 -0500

    Fix lake borders, document next steps

--- src/components/Landmasses.tsx
+++ src/components/Landmasses.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent, useCallback } from 'react';
-import { BlurFilter, Graphics as GraphicsType, Texture } from 'pixi.js';
+import { Graphics as GraphicsType, Texture } from 'pixi.js';
 import { Container, Graphics, useApp, withFilters } from '@pixi/react';
 import { GlowFilter } from '@pixi/filter-glow';
 import { OutlineFilter } from '@pixi/filter-outline';
@@ -9,7 +9,7 @@ import { biomeColor, BiomeIndexes } from '../data/biomes.ts';
 
 import oceanPattern from '../assets/ocean_pattern1.png';
 import { LakeColors } from '../data/features.ts';
-import { drawCurvedLine } from '../pixiUtils/draw.ts';
+import { drawCurvedLine, drawD3ClosedCurve } from '../pixiUtils/draw.ts';
 
 const oceanTexture = Texture.from(oceanPattern);
 
@@ -29,6 +29,8 @@ export const Landmasses: FunctionComponent<LandmassesProps> = ({
 
   // TODO: Extract all this logic when the drawing is finalized
   const drawOcean = useCallback(
+    // TODO: Convert the ocean into a sprite so we can apply the coastline filters to it
+    // https://stackoverflow.com/questions/50940737/how-to-convert-a-graphic-to-a-sprite-in-pixijs
     (g: GraphicsType) => {
       if (!physicalMap) {
         return;
@@ -39,24 +41,8 @@ export const Landmasses: FunctionComponent<LandmassesProps> = ({
         physicalMap.cells.pathPoints.coastlines.forEach(path => {
           g.beginFill(0x000000);
 
-          const [start, ...rest] = path;
-          g.moveTo(start[0], start[1]);
+          drawCurvedLine(g, path);
 
-          for (let i = 0; i < rest.length - 2; i++) {
-            const xc = (rest[i][0] + rest[i + 1][0]) / 2;
-            const yc = (rest[i][1] + rest[i + 1][1]) / 2;
-            g.quadraticCurveTo(rest[i][0], rest[i][1], xc, yc);
-          }
-
-          // curve through the last two points
-          g.quadraticCurveTo(
-            rest[rest.length - 2][0],
-            rest[rest.length - 2][1],
-            rest[rest.length - 1][0],
-            rest[rest.length - 1][1]
-          );
-
-          g.closePath();
           g.endFill();
         });
 
@@ -92,22 +78,7 @@ export const Landmasses: FunctionComponent<LandmassesProps> = ({
       physicalMap.cells.pathPoints.coastlines.forEach(path => {
         g.lineStyle(2, 0x808080, 1, 0.5);
 
-        const [start, ...rest] = path;
-        g.moveTo(start[0], start[1]);
-
-        for (let i = 0; i < rest.length - 2; i++) {
-          const xc = (rest[i][0] + rest[i + 1][0]) / 2;
-          const yc = (rest[i][1] + rest[i + 1][1]) / 2;
-          g.quadraticCurveTo(rest[i][0], rest[i][1], xc, yc);
-        }
-
-        // curve through the last two points
-        g.quadraticCurveTo(
-          rest[rest.length - 2][0],
-          rest[rest.length - 2][1],
-          rest[rest.length - 1][0],
-          rest[rest.length - 1][1]
-        );
+        drawD3ClosedCurve(g, path);
 
         g.closePath();
       });
@@ -149,6 +120,7 @@ export const Landmasses: FunctionComponent<LandmassesProps> = ({
   );
 
   const drawCells = useCallback(
+    // TODO: Convert the cells to a sprite so we can mask it for lakes and other things
     (g: GraphicsType) => {
       if (!physicalMap) {
         return;
@@ -197,6 +169,8 @@ export const Landmasses: FunctionComponent<LandmassesProps> = ({
     (g: GraphicsType) => (featureID: string, points: Point[]) => {
       g.clear();
 
+      // TODO: draw lakes over the map. If masked properly with another lake, it should render in place
+      // TODO: without issues. Check if we could scale a lake a little to make it overlap all possible cells.
       const lakeFeature = physicalMap.features[
         parseInt(featureID)
       ] as unknown as LakeFeature;
@@ -205,7 +179,7 @@ export const Landmasses: FunctionComponent<LandmassesProps> = ({
       g.beginFill(colorData.fill, colorData.fillAlpha);
       g.lineStyle(colorData.outlineWidth, colorData.outline, 1, 0.5);
 
-      drawCurvedLine(g, 0.5, points);
+      drawD3ClosedCurve(g, points);
       g.endFill();
     },
     [physicalMap]
--- src/pixiUtils/draw.ts
+++ src/pixiUtils/draw.ts
@@ -1,77 +1,41 @@
 import { Graphics as GraphicsType } from 'pixi.js';
+import * as d3Shape from 'd3-shape';
 
 import { Point } from '../types/grid.ts';
 
-/**
- * given an array of x,y's, return distance between any two,
- * note that i and j are indexes to the points, not directly into the array.
- */
-const distanceBetweenPoints = (point1: Point, point2: Point) => {
-  return Math.sqrt(
-    Math.pow(point2[0] - point1[0], 2) + Math.pow(point2[1] - point1[1], 2)
-  );
-};
+export const drawCurvedLine = (g: GraphicsType, points: Point[]) => {
+  const [start, ...rest] = points;
+  g.moveTo(start[0], start[1]);
 
-const getControlPoints = (
-  tension: number,
-  point1: Point,
-  point2: Point,
-  point3: Point
-): [Point, Point] => {
-  const d01 = distanceBetweenPoints(point1, point2);
-  const d12 = distanceBetweenPoints(point2, point3);
-  const fa = (tension * d01) / (d01 + d12); // scaling factor for triangle Ta
-  const fb = (tension * d12) / (d01 + d12); // ditto for Tb, simplifies to fb=t-fa
-  const p1x = point2[0] - fa * (point3[0] - point1[0]); // x2-x0 is the width of triangle T
-  const p1y = point2[1] - fa * (point3[1] - point1[1]); // y2-y0 is the height of T
-  const p2x = point2[0] + fb * (point3[0] - point1[0]);
-  const p2y = point2[1] + fb * (point3[1] - point1[1]);
-
-  return [
-    [p1x, p1y],
-    [p2x, p2y],
-  ];
-};
-
-export const drawCurvedLine = (
-  g: GraphicsType,
-  tension: number,
-  points: Point[]
-) => {
-  // There will be two control points for each "middle" point, 1 ... len-2e
-  const controlPointsArray: [Point, Point][] = [];
-  for (let i = 0; i < points.length - 2; i++) {
-    controlPointsArray.push(
-      getControlPoints(tension, points[i], points[i + 1], points[i + 2])
-    );
+  for (let i = 0; i < rest.length - 2; i++) {
+    const xc = (rest[i][0] + rest[i + 1][0]) / 2;
+    const yc = (rest[i][1] + rest[i + 1][1]) / 2;
+    g.quadraticCurveTo(rest[i][0], rest[i][1], xc, yc);
   }
 
-  const [first, second] = points;
-  controlPointsArray.push(
-    getControlPoints(
-      tension,
-      points[points.length - 2],
-      points[points.length - 1],
-      first
-    )
-  );
-  controlPointsArray.push(
-    getControlPoints(tension, points[points.length - 1], first, second)
+  // curve through the last two points
+  g.quadraticCurveTo(
+    rest[rest.length - 2][0],
+    rest[rest.length - 2][1],
+    rest[rest.length - 1][0],
+    rest[rest.length - 1][1]
   );
 
-  g.moveTo(first[0], first[1]);
+  // Then curve back to start
+  g.quadraticCurveTo(
+    rest[rest.length - 1][0],
+    rest[rest.length - 1][1],
+    start[0],
+    start[1]
+  );
+};
 
-  // for all middle points, connect with bezier
-  points.forEach((middlePoint, index) => {
-    const controlPoints = controlPointsArray[index];
-
-    g.bezierCurveTo(
-      controlPoints[0][0],
-      controlPoints[0][1],
-      controlPoints[1][0],
-      controlPoints[1][1],
-      middlePoint[0],
-      middlePoint[1]
-    );
-  });
+export const drawD3ClosedCurve = (g: GraphicsType, points: Point[]) => {
+  d3Shape
+    .line()
+    .curve(d3Shape.curveBasisClosed)
+    // The pixijs graphics don't fully cover the features of the canvas, but the curves only use `moveTo` and
+    // `bezierCurveTo`, which the pixijs graphics API fully supports. We force a conversion and use d3 to draw the
+    // lines, which should allow us to use their solid algorithms.
+    .context(g as unknown as CanvasRenderingContext2D)(points);
 };
